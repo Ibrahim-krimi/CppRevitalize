7d81a90335d5c06cb6d6b182089f79782d8d53ed
Ibrahim Krimi <ibrahimkrimi2@gmail.com>
2024-11-11 23:27:41 +0100

    add structure

--- Project1/math.cpp
+++ Project1/math.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <cmath>
-
+#include <vector>
+#include <string>
 using namespace std;
 
-int main() {
-	string cars[4] = { "Volvo","bmw","Ford","fiat" };
-	for(const string car : cars)
+int ex1() {
+	//string cars[4] = { "Volvo","bmw","Ford","fiat" };
+	vector <string> cars = { "Volvo","Buggati","Aston martin" };
+	for (const string car : cars)
 	{
 		cout << car << endl; 
 	}
+
+	cars.push_back("Tesla");
+	
+	for (const string car : cars) {
+		cout << car << endl;
+	}
+	cout << cars.size() << endl;
+	cout << sizeof(cars) << endl ;
+
 
 	return 0;
 }
---
+++ Project1/structure.cpp
@@ -0,0 +1,22 @@
+#include <ostream>
+ 
+using namespace std;
+struct carType {
+	string brand;
+	string model;
+	int year;
+} car1, car2;
+
+
+int main() {
+
+
+	car1.brand = "Tesla";
+	car1.model = "X";
+	car1.year = 2022;
+
+	carType car3;
+	car3 = car1;
+
+	return 0;
+}
